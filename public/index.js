e0331f04040ecf4a5e9f37f160566c6636b2f970
Neil <iccir919@gmail.com>
2025-12-12 09:15:02 -0800

    handle form reset

--- public/index.js
+++ public/index.js
@@ -1,8 +1,10 @@
+let goldPrice = null
 const eventSource = new EventSource("/price/live")
 
 const priceDisplay = document.getElementById("price-display")
 const connectionStatus = document.getElementById("connection-status")
 
+const form = document.querySelector("form")
 const investmentAmount = document.getElementById("investment-amount")
 const formFootnote = document.getElementsByClassName("footnote")[0]
 
@@ -13,8 +15,9 @@ eventSource.onmessage = (event) => {
     connectionStatus.textContent = "Live Price 🟢"
 
     const data = JSON.parse(event.data)
-    const price = data.price
-    priceDisplay.textContent = price
+    console.log(data)
+    goldPrice = data.price
+    priceDisplay.textContent = goldPrice
 }
 
 eventSource.onerror = () => {
@@ -27,7 +30,11 @@ eventSource.onerror = () => {
 document.getElementById("invest-btn").addEventListener("click", async (e) => {
     e.preventDefault()
 
+
     try {
+        const amountPaid = Number(investmentAmount.value)
+        if (amountPaid <= 0) throw new Error("Amount paid is below 0")
+
         const response = await fetch("/invest", {
             method: "POST",
             headers: {
@@ -35,13 +42,15 @@ document.getElementById("invest-btn").addEventListener("click", async (e) => {
             },
             body: JSON.stringify({
                  timestamp: new Date(),
-                 amount: Number(investmentAmount.value),
-                 price: 0.6 
+                 "amount paid": amountPaid,
+                 "gold price": goldPrice,
+                 "gold sold": amountPaid / goldPrice
             })
         })
 
         if (response.ok) {
             dialog.showModal()
+            formFootnote.textContent = "* 1oz = 1 troy ounce of 24 Carat Gold"
         } else {
             console.log(response)
             throw new Error(response.statusText)
@@ -56,4 +65,5 @@ document.getElementById("invest-btn").addEventListener("click", async (e) => {
 
 dialogBtn.addEventListener("click", () => {
     dialog.close()
+    form.reset()
 })
